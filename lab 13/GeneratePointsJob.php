36ed063cd0dfa2c482aca13b8684f303db8cb886
smachyloooo <41301896+smachylooo@users.noreply.github.com>
2021-05-12 21:00:27 +0300

    Add files via upload

---
+++ lab 13/GeneratePointsJob.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Jobs\GenerateCatalog;
+
+
+class GeneratePointsJob extends AbstractJob
+{
+
+    public function handle()
+    {
+        $f = 1 / 0; //симулюємо помилку
+
+        parent::handle();
+    }
+
+}
+
