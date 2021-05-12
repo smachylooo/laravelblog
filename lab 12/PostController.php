fbe0fa58a1360c4073465e5a243695a6ea34b42f
smachyloooo <41301896+smachylooo@users.noreply.github.com>
2021-05-12 20:34:11 +0300

    Add files via upload

---
+++ lab 12/PostController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
+use App\Repositories\BlogPostRepository;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use App\Models\BlogPost;
+use App\Http\Requests\BlogPostCreateRequest;
+
+class PostController extends BaseController
+{ /**
+ * @var BlogPostRepository
+ */
+    private $blogPostRepository;
+
+    /**
+     * @var BlogCategoryRepository
+     */
+    private $blogCategoryRepository; // властивість через яку будемо звертатись в репозиторій категорій
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->blogPostRepository = app(BlogPostRepository::class);//app вертає об'єкт класа
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {    $paginator = $this->blogPostRepository->getAllWithPaginate();
+//dd($paginator);
+        return view('blog.admin.posts.index', compact('paginator'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {$item = new BlogPost();
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+
+
+        return view('blog.admin.posts.edit', compact('item', 'categoryList'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BlogPostCreateRequest $request)
+    {
+        $data = $request->input(); //отримаємо масив даних, які надійшли з форми
+
+        $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
+
+        if ($item) {
+            $job = new BlogPostAfterCreateJob($item);
+            $this->dispatch($job);
+            return redirect()
+                ->route('blog.admin.posts.edit', [$item->id])
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {                         //помилка, якщо репозиторій не знайде наш ід
+            abort(404);
+        }
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+
+        return view('blog.admin.posts.edit', compact('item', 'categoryList'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(BlogPostUpdateRequest $request, $id)
+    {
+        //$id = 1000;
+        $item = $this->blogPostRepository->getEdit($id);
+        //dd($item);
+        if (empty($item)) { //якщо ід не знайдено
+            return back() //redirect back
+            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
+            ->withInput(); //повернути дані
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+        //dd($data);
+        //dd(__Method__,$item, $data);
+
+        $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.posts.edit', $item->id)
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->with(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = BlogPost::destroy($id); //софт деліт, запис лишається
+
+        //$result = BlogPost::find($id)->forceDelete(); //повне видалення з БД
+
+        if ($result) {
+            BlogPostAfterDeleteJob::dispatch($id)->delay(20);
+            return redirect()
+                ->route('blog.admin.posts.index')
+                ->with(['success' => "Запис id[$id] видалено"]);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка видалення']);
+        }
+    }
+}
