81685719b778dca8d9ddcbe1e605389b311285b5
smachyloooo <41301896+smachylooo@users.noreply.github.com>
2021-05-12 20:03:31 +0300

    Add files via upload

---
+++ lab 10/BlogPost.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{ use SoftDeletes;
+    const UNKNOWN_USER = 1;
+    protected $fillable
+        = [
+            'title',
+            'slug',
+            'category_id',
+            'excerpt',
+            'content_raw',
+            'is_published',
+            'published_at',
+        ];
+
+    /**
+     * Категорія статті
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        //стаття належить категорії
+        return $this->belongsTo(BlogCategory::class);
+    }
+    /**
+     * Автор статті
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        //стаття належить користувачу
+        return $this->belongsTo(User::class);
+    }
+    use HasFactory;
+}
---
+++ lab 10/BlogPostObserver.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class BlogPostObserver
+{/**
+    * Обробка перед створенням запису.
+    *
+    * @param  BlogPost  $blogPost
+    *
+    */
+    public function creating(BlogPost $blogPost)
+    {
+        $this->setPublishedAt($blogPost);
+
+        $this->setSlug($blogPost);
+
+        $this->setHtml($blogPost);
+
+        $this->setUser($blogPost);
+    }
+
+ /**
+     * Встановлюємо значення полю content_html з поля content_raw.
+     *
+     * @param BlogPost $blogPost
+     */
+    protected function setHtml(BlogPost $blogPost)
+    {
+        if ($blogPost->isDirty('content_raw')) {
+            //Тут треба зробити генерацію markdown -> html
+            $blogPost->content_html = $blogPost->content_raw;
+        }
+    }
+
+    /**
+     * Якщо user_id не вказано, то встановимо юзера 1.
+     *
+     * @param BlogPost $blogPost
+     */
+    protected function setUser(BlogPost $blogPost)
+    {
+
+        $blogPost->user_id = auth()->id() ?? BlogPost::UNKNOWN_USER;
+
+    }
+
+    /**
+    * Обробка перед оновленням запису.
+    *
+    * @param  BlogPost  $blogPost
+    *
+    */
+    public function updating(BlogPost $blogPost)
+    {
+        $this->setPublishedAt($blogPost);
+        $this->setSlug($blogPost);
+    }
+
+    /**
+     * якщо поле published_at порожнє і нам прийшло 1 в ключі is_published,
+     * то генеруємо поточну дату
+     *
+     * @param BlogPost $blogPost
+     */
+    protected function setPublishedAt(BlogPost $blogPost)
+    {
+        if (empty($blogPost->published_at) && $blogPost->is_published) {
+            $blogPost->published_at = Carbon::now();
+        }
+    }
+
+    /**
+     * якщо псевдонім порожній
+     * то генеруємо псевдонім
+     *
+     * @param BlogPost $blogPost
+     */
+    protected function setSlug(BlogPost $blogPost)
+    {
+        if (empty($blogPost->slug)) {
+            $blogPost->slug = Str::slug($blogPost->title);
+        }
+    }
+
+    /**
+     * Handle the BlogPost "created" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function created(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function deleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "restored" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function restored(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "force deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function forceDeleted(BlogPost $blogPost)
+    {
+        //
+    }
+}
