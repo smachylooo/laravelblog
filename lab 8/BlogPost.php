dd0e1c8de979d00c9a75d8d81bc1023e09eeb6c9
smachyloooo <41301896+smachylooo@users.noreply.github.com>
2021-05-12 19:57:46 +0300

    Add files via upload

---
+++ lab 8/BlogPost.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class BlogPost extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable
+        = [
+            'title',
+            'slug',
+            'category_id',
+            'excerpt',
+            'content_raw',
+            'is_published',
+            'published_at',
+            'user_id',
+        ];
+
+    /**
+     * Категорія статті
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        //стаття належить категорії
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    /**
+     * Автор статті
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        //стаття належить користувачу
+        return $this->belongsTo(User::class);
+    }
+
+}
